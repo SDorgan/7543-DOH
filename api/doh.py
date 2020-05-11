84e4dc835e9a88d3f6c7a033b85679375e8e8b57
Hernán de la Fuente <hernan.de@mercadolibre.com>
2020-05-11 12:52:00 -0300

    agrego firma de metodos

---
+++ api/doh.py
@@ -0,0 +1,28 @@
+
+def get_domain(domain):
+    print(domain)
+    pass
+
+
+def get_custom_domains(q):
+    print(q)
+    pass
+
+
+def post_custom_domain(**kwargs):
+    body = kwargs.get('body')
+    print(body)
+    pass
+
+
+def put_custom_domain(domain, **kwargs):
+    print(domain)
+    body = kwargs.get('body')
+    print(body)
+    pass
+
+
+def delete_custom_domain(domain):
+    print(domain)
+    pass
+
